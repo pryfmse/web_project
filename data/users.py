926a0bdc3b4d9ee5c33a0d91c711a20575e08a39
Smart developer <developer@yandex.ru>
2024-04-08 05:16:36 +0700

    .

---
+++ data/users.py
@@ -0,0 +1,50 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from .db_session import SqlAlchemyBase
+
+
+# ##### Это надо отправить -- начало{
+# Модель Марсиане
+
+class Jobs(SqlAlchemyBase):
+    __tablename__ = 'jobs'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    job = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    work_size = sqlalchemy.Column(sqlalchemy.Integer, nullable=True, default=0)
+    collaborators = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    start_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+    end_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+    is_finished = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
+    team_leader = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
+    user = orm.relationship('User')
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    age = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    position = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    speciality = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    address = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    modified_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+
+    jobs = orm.relationship("Jobs", back_populates='user')
+
+# ##### Это надо отправить -- конец}
+    def __repr__(self):
+        return f'<Colonist> {self.id} {self.surname} {self.name}'
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
