35e5b73ef8ee0a674d8ca3dc00c8406e6d835e4d
Smart developer <developer@yandex.ru>
2024-04-30 06:35:39 +0700

    .

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import base64
 from random import randint
 
 import sqlalchemy
@@ -115,14 +116,17 @@ def res_user_inf():
     db_sess = db_session.create_session()
     query = db_sess.query(__all_models.InfResults).filter(__all_models.InfResults.login == current_user.login)
     results = list(db_sess.execute(query))[0][0]
-    n = len(list(filter(lambda x: x >= 70, [results.t1, results.t2, results.t3, results.t4, results.t5, results.t6,
-                                            results.t7, results.t8, results.t9, results.t10, results.t11, results.t12,
-                                            results.t13, results.t14, results.t15, results.t16, results.t17,
-                                            results.t18, results.t19, results.t20, results.t21, results.t22,
-                                            results.t23,
-                                            results.t24, results.t25, results.t26a, results.t26b, results.t27a,
-                                            results.t27b])))
-    return render_template('res_user_inf.html', user=results, n_1=n, n_2=trans_inf[n])
+    results = [results.t1, results.t2, results.t3, results.t4, results.t5, results.t6,
+               results.t7, results.t8, results.t9, results.t10, results.t11, results.t12,
+               results.t13, results.t14, results.t15, results.t16, results.t17,
+               results.t18, results.t19, results.t20, results.t21, results.t22, results.t23,
+               results.t24, results.t25, results.t26a, results.t26b, results.t27a,
+               results.t27b]
+    for x in range(len(results)):
+        m = list(map(int, results[x].split()))
+        results[x] = int((sum(m) / len(m)) * 100)
+    n = len(list(filter(lambda x: x >= 70, results)))
+    return render_template('res_user_inf.html', user=results, n_1=n, n_2=trans_inf[n], prof=current_user.login)
 
 
 @app.route('/res_user_math')
@@ -130,10 +134,13 @@ def res_user_math():
     db_sess = db_session.create_session()
     query = db_sess.query(__all_models.MathResults).filter(__all_models.MathResults.login == current_user.login)
     results = list(db_sess.execute(query))[0][0]
-    n = len(list(filter(lambda x: x >= 70, [results.t1, results.t2, results.t3, results.t4, results.t5, results.t6,
-                                            results.t7, results.t8, results.t9, results.t10, results.t11,
-                                            results.t12])))
-    return render_template('res_user_math.html', user=results, n_1=n, n_2=trans_math[n])
+    results = [results.t1, results.t2, results.t3, results.t4, results.t5, results.t6,
+               results.t7, results.t8, results.t9, results.t10, results.t11, results.t12]
+    for x in range(len(results)):
+        m = list(map(int, results[x].split()))
+        results[x] = int((sum(m) / len(m)) * 100)
+    n = len(list(filter(lambda x: x >= 70, results)))
+    return render_template('res_user_math.html', user=results, n_1=n, n_2=trans_math[n], prof=current_user.login)
 
 
 @app.route('/res_admin_math', methods=['POST', 'GET'])
@@ -148,11 +155,16 @@ def res_admin1():
                 results = list(session.execute(query))[0][0]
                 n = [results.t1, results.t2, results.t3, results.t4, results.t5, results.t6,
                      results.t7, results.t8, results.t9, results.t10, results.t11, results.t12]
+                for x in range(len(n)):
+                    m = list(map(int, n[x].split()))
+                    n[x] = int((sum(m) / len(m)) * 100)
                 for j in range(len(graphik)):
                     graphik[j].append(n[j])
                 query = session.query(__all_models.Variant).filter(__all_models.Variant.login == i)
                 if list(session.execute(query)):
-                    table.append((i, sum(n) // 12, list(session.execute(query))))
+                    var = list(session.execute(query))[0][0]
+                    if var.obj == 'math':
+                        table.append((i, sum(n) // 12, var.ball1))
                 else:
                     table.append((i, sum(n) // 12, 0))
             graphik = [sum(k) // len(k) for k in graphik]
@@ -199,11 +211,16 @@ def res_admin2():
                      results.t18, results.t19, results.t20, results.t21, results.t22, results.t23,
                      results.t24, results.t25, results.t26a, results.t26b, results.t27a,
                      results.t27b]
+                for x in range(len(n)):
+                    m = list(map(int, n[x].split()))
+                    n[x] = int((sum(m) / len(m)) * 100)
                 for j in range(len(graphik)):
                     graphik[j].append(n[j])
                 query = session.query(__all_models.Variant).filter(__all_models.Variant.login == i)
                 if list(session.execute(query)):
-                    table.append((i, sum(n) // 29, list(session.execute(query))))
+                    var = list(session.execute(query))[0][0]
+                    if var.obj == 'inf':
+                        table.append((i, sum(n) // 29, var.ball1))
                 else:
                     table.append((i, sum(n) // 29, 0))
             graphik = [sum(k) // len(k) for k in graphik]
@@ -259,8 +276,14 @@ def inf_main():
         for i in request.form.getlist('inf'):
             results = list(
                 session.execute(session.query(__all_models.InfTasks).filter(__all_models.InfTasks.number == int(i))))
-            a.append(results[randint(0, len(results)) - 1][0])
-        print(a[0].file)
+            k = results[randint(0, len(results)) - 1][0]
+            if k.task_picture:
+                with open(k.task_picture, "rb") as image2string:
+                    conv_str = base64.b64encode(image2string.read())
+                k.task_picture = conv_str
+                session.merge(k)
+                session.commit()
+            a.append(k)
         return render_template('decision.html', lst=a, object='inf')
     return render_template('inf_main.html')
 
@@ -270,27 +293,63 @@ def decision(obj):
     session = db_session.create_session()
     if request.method == 'GET':
         a = []
+        photo = {}
         if obj == 'inf':
             for i in range(1, 28):
                 results = list(
                     session.execute(session.query(__all_models.InfTasks).filter(__all_models.InfTasks.number == i)))
-                a.append(results[randint(0, len(results)) - 1][0])
+                k = results[randint(0, len(results)) - 1][0]
+                if k.task_picture:
+                    with open('static/img/t' + str(k.number) + '.png', 'wb') as file:
+                        file.write(k.task_picture)
+                        photo[k.number] = 'static/img/t' + str(k.number) + '.png'
+                a.append(k)
         elif obj == 'math':
             for i in range(1, 13):
                 results = list(
                     session.execute(session.query(__all_models.MathTasks).filter(__all_models.MathTasks.number == i)))
-                a.append(results[randint(0, len(results)) - 1][0])
-        return render_template('decision.html', lst=a, object=obj)
+                k = results[randint(0, len(results)) - 1][0]
+                if k.task_picture:
+                    with open('static/img/t' + str(k.number) + '.png', 'wb') as file:
+                        file.write(k.task_picture)
+                        photo[k.number] = 'static/img/t' + str(k.number) + '.png'
+                a.append(k)
+        return render_template('decision.html', lst=a, object=obj, photo=photo)
     elif request.method == 'POST':
         res = []
         summ = 0
+        if current_user.is_authenticated:
+            if obj == 'math':
+                query = session.query(__all_models.MathResults).filter(
+                    __all_models.MathResults.login == current_user.login)
+                user = list(session.execute(query))[0][0]
+                task = {'1': user.t1, '2': user.t2, '3': user.t3, '4': user.t4, '5': user.t5, '6': user.t6,
+                        '7': user.t7, '8': user.t8, '9': user.t9, '10': user.t10, '11': user.t11, '12': user.t12}
+            else:
+                query = session.query(__all_models.InfResults).filter(
+                    __all_models.InfResults.login == current_user.login)
+                user = list(session.execute(query))[0][0]
+                task = {'1': user.t1, '2': user.t2, '3': user.t3, '4': user.t4, '5': user.t5, '6': user.t6,
+                        '7': user.t7, '8': user.t8, '9': user.t9, '10': user.t10, '11': user.t11, '12': user.t12,
+                        '13': user.t13, '14': user.t14, '15': user.t15, '16': user.t16, '17': user.t17, '18': user.t18,
+                        '19': user.t19, '20': user.t20, '21': user.t21, '22': user.t22, '23': user.t23, '24': user.t24,
+                        '25': user.t25, '26': user.t26a, '27': user.t27a}
         for i in request.form.keys():
             a, b = i.split('_')
             v = 1 if b == request.form[i] else 0
             summ += v
             res.append((a, v))
             if current_user.is_authenticated:
-                pass
+                k = task[a].split() + [str(v)]
+                task[a] = ' '.join(k)
+        if current_user.is_authenticated:
+            if (obj == 'math' and len(res) == 12) or (obj == 'inf' and len(res) == 27):
+                var = __all_models.Variant()
+                var.login = current_user.login
+                var.obj = obj
+                var.ball1 = summ
+                session.add(var)
+                session.commit()
         return render_template('res.html', res=res, itog=summ)
 
 
